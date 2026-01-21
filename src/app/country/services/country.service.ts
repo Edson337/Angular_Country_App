66f3219d200d9dbe3694e7fcd8cac181ba19f6c1
Edson733 <20213tn034@utez.edu.mx>
2026-01-21 15:39:40 -0600

    CountryService - Interfaces

---
+++ src/app/country/services/country.service.ts
@@ -0,0 +1,16 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const API_URL = 'https://restcountries.com/v3.1';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CountryService {
+  private http = inject(HttpClient);
+
+  searchByCapital(query: string) {
+    query = query.toLowerCase();
+    return this.http.get(`${API_URL}/capital/${query}`);
+  }
+}
