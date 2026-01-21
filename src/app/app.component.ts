462bd5de727cb43787f0a14a39d554b7d625e927
Edson733 <20213tn034@utez.edu.mx>
2026-01-20 19:04:01 -0600

    DaisyUI - Componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
 })
 export class AppComponent {}
