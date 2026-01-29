dc6eec11ff50c3a03d2bf3bcb12775108c42406c
Edson733 <20213tn034@utez.edu.mx>
2026-01-29 13:28:18 -0600

    RxResource

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject, resource, signal } from '@angular/core';
-import { firstValueFrom } from 'rxjs';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { firstValueFrom, of } from 'rxjs';
 import { SearchInputComponent } from '../../components/search-input/search-input.component';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { CountryService } from '../../services/country.service';
@@ -13,11 +14,42 @@ export class ByCapitalPageComponent {
   countryService = inject(CountryService);
   query = signal('');
 
-  countryResource = resource({
+  countryResource = rxResource({
     request: () => ({query: this.query()}),
-    loader: async({request}) => {
-      if (!request.query) return [];
-      return await firstValueFrom(this.countryService.searchByCapital(request.query));
+    loader: ({request}) => {
+      if (!request.query) return of([]);
+      return this.countryService.searchByCapital(request.query);
     }
   });
+
+  // countryResource = resource({
+  //   request: () => ({query: this.query()}),
+  //   loader: async({request}) => {
+  //     if (!request.query) return [];
+  //     return await firstValueFrom(this.countryService.searchByCapital(request.query));
+  //   }
+  // });
+
+  // isLoading = signal(false);
+  // isError = signal<string | null>(null);
+  // countries = signal<Country[]>([]);
+
+  // onSearch(query: string) {
+  //   if (this.isLoading()) return;
+
+  //   this.isLoading.set(true);
+  //   this.isError.set(null);
+
+  //   this.countryService.searchByCapital(query).subscribe({
+  //     next: (countries) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set(countries);
+  //     },
+  //     error: (err) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set([]);
+  //       this.isError.set(err);
+  //     },
+  //   });
+  // }
 }
